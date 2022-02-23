89a36dbdf816c12e4360dd8638e96a3cdf50afa7
Himanshu Tomar <himanshutomar3031@gmail.com>
2022-02-24 03:06:50 +0530

    Added users to the room

--- src/RoomPage/VideoSection/VideoSection.js
+++ src/RoomPage/VideoSection/VideoSection.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import VideoButtons from "./VideoButtons";
+import Videos from "./Videos";
 
 const VideoSection = () => {
-  return <div></div>;
+  const [room, setRoom] = useState(null);
+
+  return (
+    <div className="video_section_container">
+      <Videos room={room} setRoom={setRoom} />
+      <VideoButtons room={room} />
+    </div>
+  );
 };
 
 export default VideoSection;
